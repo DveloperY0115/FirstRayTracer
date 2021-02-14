7f710d597065f331cd62b67bf64744d4ec242c5b
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-15 01:13:09 +0900

    feat(rt-next-week): Implement getters of class 'aabb'

--- src/cpu/aabb.hpp
+++ src/cpu/aabb.hpp
@@ -10,6 +10,9 @@ public:
     aabb(const point3& a, const point3& b)
     : minimum(a), maximum(b) {}
 
+    point3 min() const { return minimum; }
+    point3 max() const { return maximum; }
+
     bool hit(const ray& r, double t_min, double t_max) const {
         for (int a = 0; a < 3; ++a) {
             auto t0 = fmin((minimum[a] - r.origin()[a]) / r.direction()[a],
@@ -18,7 +21,7 @@ public:
                            (maximum[a] - r.origin()[a]) / r.direction()[a]);
             t_min = fmax(t0, t_min);
             t_max = fmin(t1, t_max);
-            
+
             if (t_max <= t_min)
                 return false;
         }
